b0dcd846b10c10b45bf01dee7d2f07b9fad74a4e
Lebogang Maepa <lebogang.maepa@interfront.co.za>
2025-03-15 20:06:42 +0200

    initial commit

--- src/test/java/Basics/StartChrome.java
+++ src/test/java/Basics/StartChrome.java
@@ -1,11 +1,6 @@
-package Basics;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
@@ -16,28 +11,56 @@ public class StartChrome {
 
     @Test
     public void verifyLoginSuccess() throws InterruptedException {
-        driver= new ChromeDriver();
-//        driver= new FirefoxDriver();
-//        driver= new EdgeDriver();
-//        driver= new SafariDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(1000);
+        //Thread.sleep(2000);
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        //Thread.sleep(2000);
         driver.findElement(By.id("login-button")).click();
+        //String pageTitle = driver.getTitle()
+        //System.out.println("Page Title:" + pageTitle);
 
-        String ProductText= driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).getText();
-        Assert.assertEquals(ProductText,"Products");
 
-        driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).isDisplayed();
+        String ProductText = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).getText();
+        // System.out.println("The Product Text is "+ ProductText);
 
+        Assert.assertEquals(ProductText, "Products");
+        //Thread.sleep(2000);
+        //String ProductText = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).getText();
+
+        String pageTitle = driver.getTitle();
+        System.out.println("The title is "+ pageTitle);
+        Assert.assertEquals(pageTitle, "Swag Labs");
+
+
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("checkout")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("first-name")).sendKeys("thabiso");
+        Thread.sleep(2000);
+        driver.findElement(By.id("last-name")).sendKeys("ndlovu");
+        Thread.sleep(2000);
+        driver.findElement(By.id("postal-code")).sendKeys("7441");
+        Thread.sleep(2000);
+        driver.findElement(By.id("continue")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("finish")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("logout_sidebar_link")).click();
+        Thread.sleep(2000);
     }
 
     @AfterTest
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.quit();
     }
-
-
 }
+//*
