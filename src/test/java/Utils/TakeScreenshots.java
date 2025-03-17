ce0ec2fdafad081394c56968f632254a4f360d01
sibonisot <sibonisot@yahoo.com>
2025-03-17 19:41:54 +0200

    ignored some files

--- src/test/java/Utils/TakeScreenshots.java
+++ src/test/java/Utils/TakeScreenshots.java
@@ -6,9 +6,6 @@ public class TakeScreenshots {
 
     private static final String screenshotDir = System.getProperty("user.dir")+"/Screenshots";
 
-    @Test
-    public void test(){
-        System.out.println(screenshotDir);
-    }
+
 
 }
