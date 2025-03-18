40322dd2e1bf713a1ab8a117e79681dfa77b631c
User <breakathi@gmail.com>
2025-03-18 13:01:39 +0200

    created a screenshot class

--- src/test/java/Utils/TakeScreenshots.java
+++ src/test/java/Utils/TakeScreenshots.java
@@ -1,11 +1,24 @@
 package Utils;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 public class TakeScreenshots {
     private static String screenshotDir = System.getProperty("user.dir") + "/screenshots/";
-    @Test
-    public void test(){
-        System.out.println(screenshotDir);
+
+    public void takeScreenshot(WebDriver driver, String screemshotName ){
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File srcFale = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File destFile = new File(screenshotDir + screemshotName + ".png");
+        try {
+            FileUtils.copyFile(srcFale, destFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
