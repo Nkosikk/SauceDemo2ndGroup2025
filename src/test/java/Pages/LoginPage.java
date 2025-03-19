175e4855cc7794883261e51fd14a5f2875525c80
wilbertmaurayi <wilbertmaurayi@gmail.com>
2025-03-19 21:36:06 +0200

    Add element

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,13 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    WebDriver driver;
+    @FindBy(id="Use-Name")
+    WebElement username_Id;
+
 }
