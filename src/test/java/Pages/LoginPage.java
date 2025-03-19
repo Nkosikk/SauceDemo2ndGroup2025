43ea3652206fb3a6004e40ea7d33ee5421df6e1e
MA5861 <ntsakolyson@gmail.com>
2025-03-19 19:33:03 +0200

    Added enter password method

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -16,15 +16,21 @@ public class LoginPage {
     @FindBy(id = "user-name")
     WebElement username_id;
 
+    @FindBy(id = "password")
+    WebElement password_id;
+
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
-
+    //Input Username
     public void enterUsername(String username){
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(visibilityOf(username_id));
         username_id.sendKeys(username);
     }
-
+    //Input Password
+    public void enterPassword(String password) {
+        password_id.sendKeys(password);
+    }
 
 
 }
