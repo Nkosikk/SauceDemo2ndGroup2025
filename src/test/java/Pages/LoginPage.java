662214ac4a2f7b92cd0176543c0373a6d266171e
Lolo.Nkosi <nkosililly82@gmail.com>
2025-03-23 07:50:15 +0200

    added a new code for usersame, passowrd and login

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,30 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String username, String password) {
+        // Enter username
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys(username);
+
+        // Enter password
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+
+        // Click on login button
+        WebElement loginButton = driver.findElement(By.id("login"));
+        loginButton.click();
+
+    }
 }
