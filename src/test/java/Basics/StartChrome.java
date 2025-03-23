a1c46c1c51edd089b110aeb50f54de3b625e2cbf
Dineo <dineomoeti3@gmail.com>
2025-03-23 17:15:29 +0200

    added screenshots code

--- src/test/java/Basics/StartChrome.java
+++ src/test/java/Basics/StartChrome.java
@@ -3,41 +3,82 @@ package Basics;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import java.io.File;
+import java.io.IOException;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class StartChrome {
 
     WebDriver driver;
 
-    @Test
-    public void verifyLoginSuccess() throws InterruptedException {
-        driver= new ChromeDriver();
-//        driver= new FirefoxDriver();
-//        driver= new EdgeDriver();
-//        driver= new SafariDriver();
+    @BeforeTest
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
+        takeScreenshot("LoginPage");
+    }
+
+    public void takeScreenshot(String pageName) {
+        try {
+            File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(srcFile, new File("Screenshots/" + pageName + ".png"));
+        } catch (IOException e) {
+            System.out.println("Failed to take screenshot: " + pageName);
+        }
+    }
+
+    @Test
+    public void login() throws InterruptedException {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(1000);
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        takeScreenshot("HomePage");
+    }
 
-        String ProductText= driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).getText();
-        Assert.assertEquals(ProductText,"Products");
+    @Test(dependsOnMethods = "login")
+    public void captureTitle() throws InterruptedException {
+        // Get title
+        String title = driver.getTitle();
+        System.out.println("Page title is: " + title);
+        Thread.sleep(1000);
+        takeScreenshot("TitlePage");
+    }
 
-        driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/span")).isDisplayed();
+    @Test(dependsOnMethods = "login")
+    public void purchase() throws InterruptedException {
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.className("shopping_cart_link")).click();
+        takeScreenshot("CartPage");
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Dineo");
+        driver.findElement(By.id("last-name")).sendKeys("Moeti");
+        driver.findElement(By.id("postal-code")).sendKeys("1829");
+        driver.findElement(By.id("continue")).click();
+        takeScreenshot("CheckoutPage");
+        driver.findElement(By.id("finish")).click();
+        takeScreenshot("OrderConfirmationPage");
+    }
 
+    @Test(dependsOnMethods = "purchase")
+    public void logout() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("logout_sidebar_link"))).click();
+        takeScreenshot("LogoutPage");
     }
 
     @AfterTest
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.quit();
     }
-
-
 }
