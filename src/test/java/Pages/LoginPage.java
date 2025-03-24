8ce5d67bbd5ec3288c57d3a760f1e38615f0dc65
VuyelwaM <vmayola@gmail.com>
2025-03-24 18:11:27 +0200

    Added all the steps

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static java.awt.Color.red;
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class LoginPage {
@@ -21,6 +22,41 @@ public class LoginPage {
 
     @FindBy(id = "login-button")
     WebElement loginButton_id;
+
+
+    @FindBy(id ="add-to-cart-sauce-labs-t-shirt-(red)")
+            WebElement addtocart_id;
+
+    @FindBy(id = "shopping_cart_container")
+            WebElement shoppingcart_id;
+
+    @FindBy(id = "checkout")
+            WebElement checkout_id;
+
+    @FindBy(id = "first-name")
+            WebElement firstname_id;
+
+    @FindBy(id = "last-name")
+            WebElement lastname_id;
+
+    @FindBy(id = "postal-code")
+            WebElement postalcode_id;
+
+    @FindBy(id = "continue")
+            WebElement continue_id;
+
+    @FindBy(id = "finish")
+            WebElement finish_id;
+
+    @FindBy(xpath = "//*[@id=\"checkout_complete_container\"]/h2")
+            WebElement thankYouMessage_id;
+
+    @FindBy(xpath = "//span[@class='title'][contains(.,'Products')]")
+            WebElement productText_id;
+
+
+
+
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
@@ -39,4 +75,48 @@ public class LoginPage {
     public void clickLoginButton() {
         loginButton_id.click();
     }
+
+      //Click Add toCart
+    public void clickAddtoCart() {
+        addtocart_id.click();
+
+        //Click Shopping Cart
+        public void clickShoppingCart() {
+            shoppingcart_id.click();
+        }
+        //Click Checkout
+        public void clickCheckout() {
+            checkout_id.click();
+        }
+        //Enter First Name
+        public void enterFirstName(String firstname) {
+            firstname_id.sendKeys(firstname);
+        }
+        //Enter Last Name
+        public void enterLastName(String lastname) {
+            lastname_id.sendKeys(lastname);
+        }
+        //Enter Postal Code
+        public void enterPostalCode(String postalcode) {
+            postalcode_id.sendKeys(postalcode);
+        }
+        //Click Continue
+        public void clickContinue() {
+            continue_id.click();
+        }
+        //Click Finish
+        public void clickFinish() {
+            finish_id.click();
+        }
+        //Get Thank You Message
+        public String getThankYouMessage() {
+            return thankYouMessage_id.getText();
+        }
+        //Get Product Text
+        public String getProductText() {
+            return productText_id.getText();
+        }
+
+
+
 }
