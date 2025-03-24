ab6325c5659d89a99104da077f3899ccbedd4b68
NomveloZulu <gebour25@gmail.com>
2025-03-24 18:15:02 +0200

    Added landing login class

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -1,4 +1,19 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LandingPage {
+    WebDriver driver;
+
+    @FindBy (xpath = "//span[contains(.,'Products')]")
+    WebElement productsText;
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isProductsTextDisplayed(){
+        return productsText.isDisplayed();
+    }
 }
