9cfc33d00c11b064a7ce54cb16d1098f326dc6d9
Zamie <bazamilemjoli6@gmail.com>
2025-03-24 17:59:39 +0200

    first commit

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -1,4 +1,28 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+
 public class LandingPage {
+    public WebDriver driver;
+
+    @FindBy(xpath = "//span[contains(.,'Products')]")
+    WebElement productLabel_xpath;
+
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyProductLabelIsDisplayedInHomePage() {
+        new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.visibilityOf(productLabel_xpath));
+        String productText= productLabel_xpath.getText();
+        Assert.assertEquals(productText,"Products");
+    }
+
 }
--- src/test/java/Tests/purchaseItemTests.java
+++ src/test/java/Tests/purchaseItemTests.java
@@ -1,4 +1,18 @@
 package Tests;
 
-public class purchaseItemTests {
+import Pages.LoginPage;
+import Utils.BrowserFactory;
+
+public class purchaseItemTests extends BrowserFactory {
+
+    public void enterUsernameTests(){
+        //LoginPage.enterUsername();
+    }
+
+    public void enterPasswordTests(){
+        //LoginPage.enterPassword();
+
+    }
+
+
 }
