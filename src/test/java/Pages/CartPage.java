14e5feb26713ca362be367ec6c446e197dfb82ed
Masha <mabitselama@gmail.com>
2025-03-24 18:11:32 +0200

    1st test

---
+++ src/test/java/Pages/CartPage.java
@@ -0,0 +1,42 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+public class CartPage {
+
+
+    WebDriver driver;
+
+    //These are the page elements
+
+    @FindBy(id = "add-to-cart-sauce-labs-backpack")
+    WebElement item1_id;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bike-light")
+    WebElement item2_id;
+
+    @FindBy(id = "add-to-cart-sauce-labs-bolt-t-shirt")
+    WebElement item3_id;
+
+    @FindBy(xpath="//div[@id='shopping_cart_container']/a")
+    WebElement cart_id;
+
+
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Add 1st item to cart
+    public void add1stItem(String username) {
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(visibilityOf(item1_id));
+        item1_id.click();
+
+    }
+}
