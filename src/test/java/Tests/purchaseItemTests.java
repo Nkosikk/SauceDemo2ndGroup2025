572df483414508b4a8a6cf1ad74eabcd2d470869
sibonisot <sibonisot@yahoo.com>
2025-03-24 19:50:28 +0200

    called all methods in the test class

--- src/test/java/Tests/purchaseItemTests.java
+++ src/test/java/Tests/purchaseItemTests.java
@@ -1,4 +1,41 @@
 package Tests;
 
-public class purchaseItemTests {
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+
+
+@Test
+public class purchaseItemTests extends Base{
+
+
+    public void enterUsernameTests(){
+        loginPage.enterUsername("standard_user");
+    }
+
+    @Test(dependsOnMethods = "enterUsernameTests")
+    public void enterPasswordTests(){
+        loginPage.enterPassword("secret_sauce");
+    }
+
+    @Test(dependsOnMethods = "enterPasswordTests")
+    public void clickLoginTests(){
+        loginPage.clickLoginButton();
+    }
+
+    @Test(dependsOnMethods = "clickLoginTests")
+    public void verifyLoginSuccess(){
+        landingPage.verifyProductText();
+    }
+
+    @AfterTest
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+
+
+
+
+
 }
