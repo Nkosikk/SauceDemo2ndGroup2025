67602ec24a3737c9fdd946dde19316e29d6c9895
Slindokuhle Mkhwanaz <babongilempungose@gmail.com>
2025-03-25 18:46:25 +0200

    # Conflicts: #	src/test/java/Pages/LoginPage.java

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -18,20 +18,15 @@ public class LandingPage {
     @FindBy(xpath = "//*[@id='header_container']/div[2]/span")
     WebElement productText_xpath;
 
-
-    public LandingPage(WebDriver driver) {
+    public LandingPage(WebDriver driver)
+    {
         this.driver = driver;
     }
-    public void verifyProductText(){
+    public void verifyProductText()
+    {
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(visibilityOf(productText_xpath));
         productText_xpath.isDisplayed();
-
-//        String productText = productText_xpath.getText();
-//        Assert.assertEquals(productText, "Products");
+        //String productText = productText_xpath.getText();
+        //Assert.assertEquals(productText, "Products");
     }
-
-
-
-
-
 }
