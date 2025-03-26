7d4eaf5c8ce6ff677f2efafdcaea7c187bf72ef9
sithe <sithembisomthembi@gmail.com>
2025-03-26 18:12:51 +0200

    landing page

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -1,4 +1,13 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LandingPage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[@class='title'][contains(.,'Products')]")
+    WebElement productText_Xpath;
 }
