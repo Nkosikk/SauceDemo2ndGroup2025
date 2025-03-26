10f6efa3f4cd23ba58d99f5aac73f79a8de21085
Phelo <zwedalaphelo@gmail.com>
2025-03-26 18:13:55 +0200

    Added pages

--- src/test/java/Pages/LandingPage.java
+++ src/test/java/Pages/LandingPage.java
@@ -1,4 +1,31 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 public class LandingPage {
+
+  WebDriver driver;
+
+  @FindBy(xpath = "//*[@id=\"header_container\"]/div[2]/span")
+  WebElement productText_xpath;
+
+  public LandingPage(WebDriver driver) { this.driver=driver; }
+
+    public void verifyProductText(){
+      new WebDriverWait(driver, Duration.ofSeconds(10)).until(visibilityOf(productText_xpath));
+      productText_xpath.isDisplayed();
+
+      //String productText = productText_xpath.getText();
+        //Assert.assertEquals(productText, "Products");
+    }
+
+
+
 }
