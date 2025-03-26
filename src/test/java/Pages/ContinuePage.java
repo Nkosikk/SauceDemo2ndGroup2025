d6cf7df925f0bf5b474c7421a0de4eac2a5b87cc
Madela06 <Brianmadela2@gmail.com>
2025-03-26 13:29:53 +0200

    Add Continue Page

--- src/test/java/Pages/ContinuePage.java
+++ src/test/java/Pages/ContinuePage.java
@@ -22,19 +22,19 @@ public class ContinuePage {
     public ContinuePage(WebDriver driver) {
         this.driver = driver;
     }
-    public void enterfirstname(WebElement firstname_xpath, String firstname) {
-        firstname_xpath.sendKeys(firstname);
+    public void enterfirstname(WebElement firstname_id, String firstname) {
+        firstname_id.sendKeys(firstname);
 
     }
-public void enterlastname(WebElement lastname_xpath, String lastname) {
-        lastname_xpath.sendKeys(lastname);
+public void enterlastname(WebElement lastname_id, String lastname) {
+        lastname_id.sendKeys(lastname);
 
     }
-    public void enterzipcode(WebElement zipcode_xpath, String zipcode) {
-        zipcode_xpath.sendKeys(zipcode);
+    public void enterzipcode(WebElement zipcode_id, String zipcode) {
+        zipcode_id.sendKeys(zipcode);
 
     }
-    public void clickContinue(WebElement continue_xpath) {
-        continue_xpath.click();
+    public void clickContinue(WebElement continue_id) {
+        continue_id.click();
     }
 }
