aa218a5bd2812a323b16c2b93a3f56c59aeeb3df
Slindokuhle Mkhwanaz <babongilempungose@gmail.com>
2025-03-31 00:43:58 +0200

    # Conflicts: #	src/test/java/Pages/LoginPage.java

--- src/test/java/Tests/purchaseItemTests.java
+++ src/test/java/Tests/purchaseItemTests.java
@@ -3,38 +3,55 @@ package Tests;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
-
-
 @Test
 public class purchaseItemTests extends Base{
 
-
-    public void enterUsernameTests(){
+    public void enterUsernameTests()
+    {
         loginPage.enterUsername("standard_user");
     }
-
     @Test(dependsOnMethods = "enterUsernameTests")
-    public void enterPasswordTests(){
+    public void enterPasswordTests()
+    {
         loginPage.enterPassword("secret_sauce");
     }
-
     @Test(dependsOnMethods = "enterPasswordTests")
-    public void clickLoginTests(){
+    public void clickLoginTests()
+    {
         takeScreenshots.takesSnapShot(driver,"Login Page");
         loginPage.clickLoginButton();
     }
-
     @Test(dependsOnMethods = "clickLoginTests")
-    public void verifyLoginSuccess(){
+    public void verifyLoginSuccess()
+    {
         takeScreenshots.takesSnapShot(driver,"Landing Page");
         landingPage.verifyProductText();
     }
 
+    //Product Page
+    @Test
+    public void AddToCartPack()
+    {
+        productPage.addToCartPack("addtocartsaucelabsbackpack_id");
+    }
+    @Test
+    public void AddToCartLight()
+    {
+        productPage.addToCartLight("addtocartsaucelabsbacklight_id");
+    }
+    @Test
+    public void clickCartContainer()
+    {
+        takeScreenshots.takesSnapShot(driver,"Product Page");
+        productPage.clickShoppingCartContainer();
+    }
+
     //BackHomePage
-  //  @Test
-  ////  public void clickBackHomeButton(){
+   /* @Test
+     public void clickBackHomeButton()
+    {
         backhomePage.ClickBackHomeButton();
-//}
+    }*/
 
     @AfterTest
     public void closeBrowser(){
