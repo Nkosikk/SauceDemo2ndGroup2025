3401c90197bec13ff66a61688a8623717ff8e4eb
wilbertmaurayi <wilbertmaurayi@gmail.com>
2025-03-31 20:16:00 +0200

    Purchase test

--- src/test/java/Tests/purchaseItemTests.java
+++ src/test/java/Tests/purchaseItemTests.java
@@ -3,18 +3,18 @@ package Tests;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
+import static Utils.ReadFromExcel.password;
+import static Utils.ReadFromExcel.username;
+
 @Test
 public class purchaseItemTests extends Base {
 
-
     public void enterUsernameTests() {
-
         loginPage.enterUsername(readFromExcel.username);
     }
 
     @Test(dependsOnMethods = "enterUsernameTests")
     public void enterPasswordTests() {
-
         loginPage.enterPassword(readFromExcel.password);
     }
 
