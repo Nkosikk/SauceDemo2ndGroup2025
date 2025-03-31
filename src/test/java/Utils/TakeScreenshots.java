f8f3be3ef42a8a6114cfa2e827afa4d722960e2a
dikole <dikoleisaac@live.com>
2025-03-31 15:34:10 +0200

    Created Screenshot Directory

---
+++ src/test/java/Utils/TakeScreenshots.java
@@ -0,0 +1,10 @@
+package Utils;
+
+import org.testng.annotations.Test;
+
+public class TakeScreenshots {
+    private static final String screenshotDir = System.getProperty("user.dir") + "/screenshots/";
+
+    }
+
+}
