6f43d653b63492836aacf4806385fcc6610c025a
dikole <dikoleisaac@live.com>
2025-03-31 15:45:13 +0200

    Ignored not needed files

--- src/test/java/Utils/TakeScreenshots.java
+++ src/test/java/Utils/TakeScreenshots.java
@@ -5,6 +5,11 @@ import org.testng.annotations.Test;
 public class TakeScreenshots {
     private static final String screenshotDir = System.getProperty("user.dir") + "/screenshots/";
 
+    @Test
+    public void test() {
+        System.out.println(screenshotDir);
+
     }
 
-}
+    }
+
