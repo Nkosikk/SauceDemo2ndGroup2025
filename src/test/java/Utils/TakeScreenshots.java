8d31cf6a3d9194d292537ae620ce2f2b0d02b8b7
dikole <dikoleisaac@live.com>
2025-03-31 16:02:47 +0200

    Created image grabber

--- src/test/java/Utils/TakeScreenshots.java
+++ src/test/java/Utils/TakeScreenshots.java
@@ -1,13 +1,28 @@
 package Utils;
 
-import org.testng.annotations.Test;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class TakeScreenshots {
     private static final String screenshotDir = System.getProperty("user.dir") + "/screenshots/";
 
-    @Test
-    public void test() {
-        System.out.println(screenshotDir);
+    public void takeScreenshot(WebDriver driver, String screenshotName) {
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        File destination = new File(screenshotDir + screenshotName + ".png");
+
+        try {
+            FileUtils.copyFile(source, destination);
+            System.out.println("Screenshot taken: " + screenshotName);
+        } catch (IOException e) {
+            System.out.println("Failed to take screenshot: " + e.getMessage());
+        }
 
     }
 
