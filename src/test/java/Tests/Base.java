1572724908b147d2e4520eec815f4e123be49913
Slindokuhle Mkhwanaz <babongilempungose@gmail.com>
2025-04-01 22:53:43 +0200

    # Conflicts: #	src/test/java/Pages/LoginPage.java

--- src/test/java/Tests/Base.java
+++ src/test/java/Tests/Base.java
@@ -2,9 +2,14 @@ package Tests;
 
 import Pages.*;
 import Utils.BrowserFactory;
+import Utils.ReadFromExcel;
 import Utils.TakeScreenshots;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class Base
 {
@@ -29,4 +34,56 @@ public class Base
 
     BackHomePage BackHomePage = PageFactory.initElements(driver, BackHomePage.class);
 
+    ReadFromExcel readFromExcel;
+
+    {
+        try {
+            readFromExcel = new ReadFromExcel();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    TakeScreenshots takeScreenshots = new TakeScreenshots();
+
 }
+
+/*
+package Tests;
+
+import Pages.LandingPage;
+import Pages.LoginPage;
+import Utils.BrowserFactory;
+import Utils.ReadFromExcel;
+import Utils.TakeScreenshots;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Base {
+
+    BrowserFactory browserFactory = new BrowserFactory();
+
+    final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.saucedemo.com/");
+
+    LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+    LandingPage landingPage = PageFactory.initElements(driver, LandingPage.class);
+
+    ReadFromExcel readFromExcel;
+
+    {
+        try {
+            readFromExcel = new ReadFromExcel();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    TakeScreenshots takeScreenshots = new TakeScreenshots();
+
+}
+
+ */
