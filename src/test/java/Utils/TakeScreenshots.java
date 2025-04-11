31ee6f775be86204a0be1221ed10292b0916d583
TaniaD <dikwenit@gmail.com>
2025-04-11 09:30:47 +0200

    ignored some files

--- src/test/java/Utils/TakeScreenshots.java
+++ src/test/java/Utils/TakeScreenshots.java
@@ -7,8 +7,5 @@ public class TakeScreenshots {
 
     private static final String screenshotDir = System.getProperty("user.dir");
 
-    @Test
-    public void test(){
-        System.out.println(screenshotDir);
-    }
+
 }
