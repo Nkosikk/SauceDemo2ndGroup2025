9adbdedd86792462fd722c825cf990229ab79e80
TaniaD <dikwenit@gmail.com>
2025-04-11 09:15:10 +0200

    Created the Directory for the screenshots

--- src/test/java/Utils/TakeScreenshots.java
+++ src/test/java/Utils/TakeScreenshots.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class TakeScreenshots {
 
 
-    private static final String screenshotDir = System.getProperty("user.dir")+;
+    private static final String screenshotDir = System.getProperty("user.dir");
 
     @Test
     public void test(){
